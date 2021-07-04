07c14bf220a6d417da71c22c90ae98acb4bcfa2c
John Reyes <john.reyes@eroad.com>
2021-07-04 14:26:06 +1200

    look it up

--- app-ecr/0.repo.login.sh
+++ app-ecr/0.repo.login.sh
@@ -1,4 +1,4 @@
 AWS_REGION=us-east-1
-AWS_ACCOUNT_ID=465521767555
+AWS_ACCOUNT_ID=`aws sts get-caller-identity | jq -r ".Account"`
 
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
