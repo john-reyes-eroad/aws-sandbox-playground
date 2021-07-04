2b452b1096272c4f079b6f7b35e82f64834406c7
John Reyes <john.reyes@eroad.com>
2021-07-04 16:16:52 +1200

    tagged image

--- app-ecr/1.image.build.sh
+++ app-ecr/1.image.build.sh
@@ -1,3 +1,6 @@
 mvn clean install
 
-docker build .
+AWS_REGION=us-east-1
+AWS_ACCOUNT_ID=`aws sts get-caller-identity | jq -r ".Account"`
+
+docker build . -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/greetings-api-repo
